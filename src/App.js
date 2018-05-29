fc5ed91daf187529c7be42b20b21302dfbd55d8f
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-29 15:15:54 -0400

    Add and style github component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Route, Switch, NavLink } from 'react-router-dom'
 
 import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
@@ -13,12 +14,12 @@ class App extends Component {
         </div>
         <ul className="navLinks">
           <li>
-            <NavLink to="/github">Github API</NavLink>
+            <NavLink to="/github">GitHub API</NavLink>
           </li>
         </ul>
         <Switch>
-          <Route path="/github" render={() => <h1>Github!</h1>} />
-          <Route render={() => <p>To get started, click one of the links above</p>} />
+          <Route path="/github" component={Github} />
+          <Route render={() => <p>To get started, click one of the links above.</p>} />
         </Switch>
       </div>
     )
